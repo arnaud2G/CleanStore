469ff8fea01b61fe93fdfc8afe85f2f5f338cd9d
Arnaud Verrier <arnaudverrier@MBP-de-Arnaud.tagattitude.local>
2018-09-27 16:53:49 +0200

    Initial Commit

---
+++ CleanStore/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CleanStore
+//
+//  Created by Arnaud Verrier on 27/09/2018.
+//  Copyright © 2018 tagattitude. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ CleanStoreTests/CleanStoreTests.swift
@@ -0,0 +1,34 @@
+//
+//  CleanStoreTests.swift
+//  CleanStoreTests
+//
+//  Created by Arnaud Verrier on 27/09/2018.
+//  Copyright © 2018 tagattitude. All rights reserved.
+//
+
+import XCTest
+@testable import CleanStore
+
+class CleanStoreTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
